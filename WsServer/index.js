6dec29f33532885d8115a12389becd3e39e75fc7
Ellementul <ananev.ivan.ur@ya.ru>
2023-01-01 18:29:24 +0500

    Added test for transport

--- WsServer/index.js
+++ WsServer/index.js
@@ -1,14 +1,21 @@
 const { WebSocketServer, OPEN } = require('ws');
+function startServer(port){
+  const wss = new WebSocketServer({ port });
 
-const wss = new WebSocketServer({ port: 8080 });
+  wss.on('connection', function connection(ws) {
+    ws.on('message', function message(data, isBinary) {
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function message(data, isBinary) {
-
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === OPEN) {
-        client.send(data, { binary: isBinary });
-      }
+      wss.clients.forEach(function each(client) {
+        if (client.readyState === OPEN) {
+          client.send(data, { binary: isBinary });
+        }
+      });
     });
   });
-});
+
+  return wss
+}
+
+module.exports = {
+  startServer 
+}
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const { WsTransport } = require('./Transport')
-const transport = new WsTransport("ws://localhost:8080")
+const transport = new WsTransport("wss://sixty-toes-thank-45-153-213-108.loca.lt")
 transport.onRecieve(console.log)
 transport.send({ test: "0987654321" })
