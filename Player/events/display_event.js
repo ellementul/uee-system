365169eb6de4fd4dde613fe3f14d9d28a4914b5b
Ellementul <ananev.ivan.ur@ya.ru>
2023-04-21 16:28:14 +0500

    Clear unused dirs

--- Player/events/display_event.js
+++
@@ -1,7 +0,0 @@
-const { EventFactory, Types } = require('@ellementul/uee')
-const type = Types.Object.Def({
-  system: "UI",
-  entity: "Display",
-  action: "Show"
-}, true)
-module.exports = EventFactory(type)
